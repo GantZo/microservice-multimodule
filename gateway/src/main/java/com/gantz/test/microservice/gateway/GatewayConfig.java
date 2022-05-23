a59f88095cb92dd2845a5bd6ce4d9332392a639a
gantz <ganz@onlinesup.com>
2022-05-23 14:57:21 +0300

    Initial Commit

---
+++ gateway/src/main/java/com/gantz/test/microservice/gateway/GatewayConfig.java
@@ -0,0 +1,18 @@
+package com.gantz.test.microservice.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/api/category/**").uri("lb://category-service/"))
+                .build();
+    }
+
+}

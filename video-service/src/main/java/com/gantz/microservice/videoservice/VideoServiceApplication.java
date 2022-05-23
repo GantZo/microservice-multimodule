74d6a950584ac8e36018a747d3415d536ed9bb38
gantz <putrify@mail.ru>
2022-05-23 15:30:59 +0300

    add: video-service + add&change jpa

---
+++ video-service/src/main/java/com/gantz/microservice/videoservice/VideoServiceApplication.java
@@ -0,0 +1,15 @@
+package com.gantz.microservice.videoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class VideoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoServiceApplication.class, args);
+    }
+
+}
